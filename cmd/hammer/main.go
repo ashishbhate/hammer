23d1dc7ed3a7e8a98ec7aa505cbc8d6565af8edf
Ashish Bhate <bhate.ashish@gmail.com>
2019-09-02 09:50:21 +0530

    add usage example

---
+++ cmd/hammer/main.go
@@ -0,0 +1,28 @@
+// The executable program is the test and example
+package main
+
+import (
+	"fmt"
+
+	"github.com/ashishbhate/hammer"
+)
+
+func main() {
+	input := make(chan string)
+	output := make(chan hammer.Result)
+	stop := make(chan struct{})
+
+	workers := hammer.WorkersAll(input, output, stop)
+
+	for _, worker := range workers {
+		go worker.Start()
+	}
+
+	go hammer.SubmitAddresses(hammer.SampleAddresses, input)
+
+	for range hammer.SampleAddresses {
+		res := <-output
+		fmt.Printf("%+v\n", res)
+	}
+	// signal exit by closing exit channel
+}
